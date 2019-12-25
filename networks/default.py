dac54738da367eb93818e22ec0dd840c252074ee
Clément Tabary <clement.tabary@musicworldmedia.com>
2019-12-25 00:10:05 -0500

    Add self attention module

--- networks/default.py
+++ networks/default.py
@@ -203,3 +203,44 @@ class Siamese(nn.Module):
 
     def forward(self, input):
         return self.model(input)
+
+
+class Self_Attn(nn.Module):
+    """ Self attention Layer (implementation from @heykeetae)"""
+
+    def __init__(self, in_dim):
+        super(Self_Attn, self).__init__()
+        self.chanel_in = in_dim
+        self.query_conv = nn.Conv2d(
+            in_channels=in_dim, out_channels=in_dim // 8, kernel_size=1)
+        self.key_conv = nn.Conv2d(
+            in_channels=in_dim, out_channels=in_dim // 8, kernel_size=1)
+        self.value_conv = nn.Conv2d(
+            in_channels=in_dim, out_channels=in_dim, kernel_size=1)
+        self.gamma = nn.Parameter(torch.zeros(1))
+
+        self.softmax = nn.Softmax(dim=-1)
+
+    def forward(self, x):
+        """
+            inputs :
+                x : input feature maps( B X C X W X H)
+            returns :
+                out : self attention value + input feature
+                attention: B X N X N (N is Width*Height)
+        """
+        m_batchsize, C, width, height = x.size()
+        proj_query = self.query_conv(x).view(
+            m_batchsize, -1, width * height).permute(0, 2, 1)
+        proj_key = self.key_conv(x).view(
+            m_batchsize, -1, width * height)
+        energy = torch.bmm(proj_query, proj_key)
+        attention = self.softmax(energy)
+        proj_value = self.value_conv(x).view(
+            m_batchsize, -1, width * height)
+
+        out = torch.bmm(proj_value, attention.permute(0, 2, 1))
+        out = out.view(m_batchsize, C, width, height)
+
+        out = self.gamma * out + x
+        return out, attention
